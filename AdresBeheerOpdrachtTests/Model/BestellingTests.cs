1470ecf256c07b35d888cf2435b1e89b48562e3e
Robin Van Welden <robin.vanwelden@student.hogent.be>
2021-01-12 17:56:08 +0100

    Tests

---
+++ AdresBeheerOpdrachtTests/Model/BestellingTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdresBeheerOpdracht.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AdresBeheerOpdracht.Exceptions;
+
+namespace AdresBeheerOpdracht.Model.Tests
+{
+    [TestClass()]
+    public class BestellingTests
+    {
+        [TestMethod()]
+        public void Test_CTR_idTijdstip()
+        {
+            int id = 5;
+            DateTime dt = DateTime.Now;
+            Bestelling b = new Bestelling(id, dt);
+
+            Assert.AreEqual(dt, b.Tijdstip);
+            Assert.AreEqual(id, b.BestellingId);
+            Assert.AreEqual(default(Boolean), b.Betaald);
+        }
+
+        [TestMethod()]
+        public void Test_CTR_idTijdstipKlant()
+        {
+            int id = 5;
+            DateTime dt = DateTime.Now;
+            Klant k = new Klant("robin", "adres ofwa");
+            Bestelling b = new Bestelling(id,k, dt);
+
+            Assert.AreEqual(dt, b.Tijdstip);
+            Assert.AreEqual(id, b.BestellingId);
+            Assert.AreEqual(default(Boolean), b.Betaald);
+            Assert.AreEqual(k, b.Klant);
+        }
+
+        [TestMethod()]
+        public void Test_VerwijderKlant()
+        {
+            int id = 5;
+            DateTime dt = DateTime.Now;
+            Klant k = new Klant("robin", "adres ofwa");
+            Bestelling b = new Bestelling(id, k, dt);
+
+            b.VerwijderKlant();
+
+            Assert.AreEqual(null, b.Klant);
+        }
+
+        [TestMethod()]
+        public void Test_ZetID_LessThenZero()
+        {
+            int id = 5;
+            DateTime dt = DateTime.Now;
+            Klant k = new Klant("robin", "adres ofwa");
+            Bestelling b = new Bestelling(id, k, dt);
+
+
+            Assert.ThrowsException<BestellingException>(() => b.ZetBestellingId(-1));
+        }
+
+        [TestMethod]
+        public void Test_ZetKlant()
+        {
+            int id = 5;
+            DateTime dt = DateTime.Now;
+            Klant k = new Klant("robin", "adres ofwa");
+            Bestelling b = new Bestelling(id, k, dt);
+
+            Klant newK = new Klant("jean", "nog een adres ofwa");
+            b.ZetKlant(newK);
+
+            Assert.AreEqual(newK, b.Klant);
+        }
+
+        [TestMethod]
+        public void Test_ZetBetaald()
+        {
+            int id = 5;
+            DateTime dt = DateTime.Now;
+            Klant k = new Klant("robin", "adres ofwa");
+            Bestelling b = new Bestelling(id, k, dt);
+
+            b.ZetBetaald();
+
+            Assert.AreEqual(true, b.Betaald);
+        }
+    }
+}
---
+++ AdresBeheerOpdrachtTests/Model/KlantTests.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdresBeheerOpdracht.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AdresBeheerOpdracht.Exceptions;
+
+namespace AdresBeheerOpdracht.Model.Tests
+{
+    [TestClass()]
+    public class KlantTests
+    {
+        [TestMethod()]
+        public void Test_CTR_NaamAdres()
+        {
+            string naam = "Robin";
+            string adres = "kortrijkstraat 60 9700 oudenaarde";
+
+            Klant k = new Klant(naam, adres);
+
+            Assert.AreEqual(naam, k.Naam);
+            Assert.AreEqual(adres, k.Adres);
+            Assert.AreEqual(default(int), k.KlantId);
+        }
+
+        [TestMethod()]
+        public void Test_CTR_NaamAdresId()
+        {
+            string naam = "Robin";
+            string adres = "kortrijkstraat 60 9700 oudenaarde";
+            int id = 100;
+
+            Klant k = new Klant(id, naam, adres);
+
+            Assert.AreEqual(naam, k.Naam);
+            Assert.AreEqual(adres, k.Adres);
+            Assert.AreEqual(id, k.KlantId);
+        }
+
+        [TestMethod()]
+        public void Test_ZetNaam_Valid()
+        {
+            string naam = "robin";
+            string newNaam = "andreas";
+            string adres = "dit is een andreas";
+            Klant k = new Klant(naam, adres);
+
+            Assert.AreEqual(naam, k.Naam);
+            Assert.AreEqual(adres, k.Adres);
+            Assert.AreEqual(default(int), k.KlantId);
+
+            k.ZetNaam(newNaam);
+
+            Assert.AreEqual(newNaam, k.Naam);
+            Assert.AreEqual(adres, k.Adres);
+            Assert.AreEqual(default(int), k.KlantId);
+        }
+
+        [TestMethod()]
+        public void ZetAdresTest()
+        {
+            string naam = "robin";
+            string adres = "dit is een andreas";
+            string adresNew = "dit is zijn nieuwe adres";
+            Klant k = new Klant(naam, adres);
+
+            Assert.AreEqual(naam, k.Naam);
+            Assert.AreEqual(adres, k.Adres);
+            Assert.AreEqual(default(int), k.KlantId);
+
+            k.ZetAdres(adresNew);
+
+            Assert.AreEqual(naam, k.Naam);
+            Assert.AreEqual(adresNew, k.Adres);
+            Assert.AreEqual(default(int), k.KlantId);
+        }
+
+        [TestMethod()]
+        public void Test_ZetNaam_ThrowKlantException()
+        {
+            string naam = "robin";
+            string naamFout = "";
+            string adres = "robin zijn adres";
+            Klant k = new Klant(naam, adres);
+
+            Assert.ThrowsException<KlantException>(() => k.ZetNaam(naamFout));
+        }
+
+        [TestMethod()]
+        public void Test_ZetAdres_ThrowKlantException()
+        {
+            string naam = "robin";
+            string adres = "robin zijn adres";
+            string adresFout = "";
+            Klant k = new Klant(naam, adres);
+
+            Assert.ThrowsException<KlantException>(() => k.ZetAdres(adresFout));
+        }
+
+        [TestMethod()]
+        public void Test_CTR_Naam_ThrowKlantException()
+        {
+            string naam = "";
+            string adres = "dit is een adres";
+            Klant k;
+
+            Assert.ThrowsException<KlantException>(()=> k = new Klant(naam,adres));
+        }
+
+        [TestMethod()]
+        public void Test_CTR_Adres_ThrowKlantException()
+        {
+            string naam = "robin";
+            string adres = "";
+            Klant k;
+
+            Assert.ThrowsException<KlantException>(() => k = new Klant(naam, adres));
+        }
+    }
+}
